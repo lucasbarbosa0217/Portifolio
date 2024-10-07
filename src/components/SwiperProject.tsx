7406a0af64a4633aefe8fc5bf9e0b5cc393729de
Lucas Barbosa <lucasbarbosa0217@gmail.com>
2024-10-07 19:41:19 -0300

    scroll fix

--- src/components/SwiperProject.tsx
+++ src/components/SwiperProject.tsx
@@ -13,7 +13,7 @@ const SwiperProject: React.FC<Props> = ({ children }) => {
   const isInView = useInView(ref, { once: true });  // A animação acontece apenas na primeira vez que o contêiner é visível
 
   return (
-    <div ref={ref} className="mySwiper bookList flex lg:grid pb-8 mt-16 px-4 md:px-0 snap-x snap-mandatory justify-start lg:justify-center lg:grid-cols-2 max-h-[40rem] overflow-y-auto  gap-y-16 gap-x-8 md:p-8  w-full overflow-x-auto">
+    <div ref={ref} className="mySwiper bookList flex lg:grid pb-8 mt-16 px-4 md:px-0 snap-x snap-mandatory justify-start lg:justify-center lg:grid-cols-2 max-h-[40rem] md:overflow-y-auto  gap-y-16 gap-x-8 md:p-8  w-full overflow-x-auto">
       {Children.map(children, (child, index) => (
         <motion.div
           key={index}
