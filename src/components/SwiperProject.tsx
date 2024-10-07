2eb41dba2972bc6b99491fe8517406f7cfebe89c
Lucas Barbosa <lucasbarbosa0217@gmail.com>
2024-10-07 19:31:45 -0300

    snap

--- src/components/SwiperProject.tsx
+++ src/components/SwiperProject.tsx
@@ -13,7 +13,7 @@ const SwiperProject: React.FC<Props> = ({ children }) => {
   const isInView = useInView(ref, { once: true });  // A animação acontece apenas na primeira vez que o contêiner é visível
 
   return (
-    <div ref={ref} className="mySwiper bookList flex lg:grid pb-8 mt-16  justify-start lg:justify-center lg:grid-cols-2 max-h-[40rem] overflow-y-auto  gap-y-16 gap-x-8 md:p-8  w-full overflow-x-auto">
+    <div ref={ref} className="mySwiper bookList flex lg:grid pb-8 mt-16 pl-4 md:pl-0 snap-x snap-mandatory justify-start lg:justify-center lg:grid-cols-2 max-h-[40rem] overflow-y-auto  gap-y-16 gap-x-8 md:p-8  w-full overflow-x-auto">
       {Children.map(children, (child, index) => (
         <motion.div
           key={index}
@@ -24,7 +24,7 @@ const SwiperProject: React.FC<Props> = ({ children }) => {
             duration: 0.5,
             ease: 'easeInOut',
           }}
-          className='flex flex-col '
+          className='flex flex-col snap-center'
         >
           {child}
         </motion.div>
